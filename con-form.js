efb269e33b386ec84c62d16b4e68059b8bb3794b
ViVyVee <112664301+ViVyVee@users.noreply.github.com>
2022-09-02 20:36:55 +0800

    Add files via upload

---
+++ con-form.js
@@ -0,0 +1,10 @@
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+      !alert("Your submission is received!"), form.reset()
+  )
+  });
